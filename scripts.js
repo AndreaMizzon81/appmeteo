cb86ea93e1d214c226a1c9afcd85eae274a01d00
AndreaMizzon81 <47173650+AndreaMizzon81@users.noreply.github.com>
2019-03-04 12:00:51 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
